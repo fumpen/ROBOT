c7589728cae7f9fdf744ea4606e7b50af9a7d5b1
Pi Raspberry <robot.hacked@gmail.com>
2016-09-29 16:05:05 +0200

    tt

--- camera.py
+++ camera.py
@@ -1,26 +1,56 @@
 from picamera.array import PiRGBArray
-from picamera import PiCamera
+import picamera
 import cv2
 import numpy as np
 import time
 
 
-cam = PiCamera()
-rawCapture = PiRGBArray(cam)
+#cam = PiCamera()
+#rawCapture = PiRGBArray(cam)
 
 
-def capture(name):
-
+def capturePerm(name):
     file = "image/" + name + '.png'
     print("Taking picture")
+
+    with picamera.PiCamera() as camera:
+	with picamera.array.PiRGBArray(camera) as output:
+	    camera.capture(output, format="bgr")
+	    img = output.array
     
-    cam.capture(rawCapture, format="bgr")
-    image = rawCapture.array
-    cv2.imwrite(file, image)
+    
+    cv2.imwrite(file, img)
     
     print("Location: " + file)
 
 
+def capture():
+    with picamera.PiCamera() as camera:
+	with picamera.array.PiRGBArray(camera) as output:
+	    camera.capture(output, format="bgr")
+	    img = output.array
+
+	    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)   
+	    mask = cv2.inRange(hsv, greenLower, greenUpper)
+	    mask = cv2.erode(mask, None, iterations=4)
+	    mask = cv2.dilate(mask, None, iterations=4)   
+	    
+	    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	    center = (0, 0)
+
+	    if(len(cnts) > 0):
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		
+		if radius > 10:
+		    cv2.circle(mask, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+		    cv2.circle(mask, center, 5, (100, 100, 100), -1) 
+	    output.truncate(0)
+    print(center)
+    return center[0]
 
 #Colors
 greenLower = np.array([50, 90, 50])
@@ -50,10 +80,11 @@ def findColor(name):
 				(0, 255, 255), 2)
 	    cv2.circle(mask, center, 5, (100, 100, 100), -1)
 	
-    
-    print(center)    
     cv2.imwrite('image/' + name + 'Color.png', mask)
+    
+    
+    return center
 
 
-findColor('fourth')
- 
+capturePerm('base')
+
--- control.py
+++ control.py
@@ -1,6 +1,8 @@
 import robot
 import moves
 import sensor
+import camera
+from time import sleep
 
 frindo = robot.Robot()
 
@@ -8,5 +10,34 @@ frindo = robot.Robot()
 BATTERY = 1
 
 
-def trial_forward_func(cm):
-    moves.scale(cm, frindo, BATTERY)
+
+    
+def evergreen():
+	
+	if sensor.frontSensor(frindo) < 300 and \
+	sensor.rightSensor(frindo) < 300 and \
+	sensor.leftSensor(frindo) < 300:
+
+		x = camera.capture()
+		sleep(5)
+		if x:
+			if x in range(368):
+				if int(110/368) * x > 15:
+					moves.turn_right(frindo, int(110/368) * x, BATTERY)
+		    
+			if x  not in range(368) and x in range(736):
+				if int(110/368) * x > 15:
+					moves.turn_left(frindo, int(110/368) * x, BATTERY)
+		    
+			moves.forwardv2(frindo, 15, BATTERY)
+		else:
+			moves.turn_left(frindo, 50, BATTERY)
+		
+		return evergreen()
+	else:
+		print('lolz')
+		return 0
+
+
+
+evergreen()
