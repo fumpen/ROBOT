e55db508665b63eb43877a2a96283c6e204be2d0
Pi Raspberry <robot.hacked@gmail.com>
2016-10-25 13:42:19 +0200

    123

--- distMeasure.py
+++ distMeasure.py
@@ -1,15 +1,53 @@
+from  __future__ import division
 import numpy as np
+import camera
 import cv2
 
 def focalLength (Z, H, h):
-    return (Z * h)/(H)
+    return float(Z * h)/float(H)
 
 def distToBox (f, H, h):
-    return f*H/h
+    return float(f*H)/float(h)
 
 def distToUnknownBox(D, p1, p2):
     return D*p1/(p2-p1)
 
-a = focalLength(3, 0.31, 40)
-print distToBox(a, 0.31, 40)
-print distToUnknownBox(0.2, 350, 351)
+pics = ["1_25m", "1_5m", "1_75m", "2m", "2_25m", "2_5m", "2_75m", "3m"]
+dist = [1.25, 1.50, 1.75, 2.00, 2.25, 2.50, 2.75, 3.00]
+focal = []
+pixel = []
+
+dummy = "demo"
+
+camera.capturePerm(dummy)
+
+camera.findColor(dummy)
+
+pix = camera.pixels(dummy)
+#print pix
+
+#foc = focalLength(, 31, pix)
+#print foc
+
+focal = 623.991935484
+
+avg_foc = 0
+
+
+for item in pics:
+    pix_tmp = camera.pixels(item)
+    pixel.append(pix_tmp)
+
+for i in range(0, len(pics)):
+    tmp = focalLength(dist[i], 0.31, pixel[i])
+    #print tmp
+    avg_foc += tmp
+
+avg_foc /= len(pics)
+
+#avg_foc =  camera.statistic_Distance(pics)
+
+print avg_foc
+
+print distToBox(avg_foc, 0.295, pix)
+# print distToUnknownBox(50, pixel[7], pixel[5])
--- video.py
+++ video.py
@@ -10,7 +10,6 @@ import robot
 import moves
 import sensor
 
-
 frindo = robot.Robot()
 
 BATTERY = 1
@@ -23,14 +22,14 @@ AVG_DISTANCE = 212
 FOCAL = (AVG_PIX * AVG_DISTANCE) / SIZE_BOX
 
 #Colors
-greenLower = np.array([40, 100, 50])
-greenUpper = np.array([89, 255, 255])
+greenLower = np.array([43, 72, 35])
+greenUpper = np.array([85, 255, 255])
 # initialize the camera and grab a reference to the raw camera capture
 
 camera = PiCamera()
 time.sleep(1) # Wait for camera
 
-camera.resolution = (640, 480)
+camera.resolution = (736, 480)
 camera.framerate = 30
 
 camera.shutter_speed = camera.exposure_speed
@@ -46,33 +45,32 @@ print "shutter_speed = ", camera.shutter_speed
 print "awb_gains = ", gain
 
 rawCapture = PiRGBArray(camera, size=camera.resolution)
- 
-# Open a window
-WIN_RF = "Frame";
-cv2.namedWindow(WIN_RF);
-cv2.moveWindow(WIN_RF, 100       , 100);
 
+# Open a window
+# WIN_RF = "Frame";
+# cv2.namedWindow(WIN_RF);
+# cv2.moveWindow(WIN_RF, 100       , 100);
 
 # allow the camera to warmup
 time.sleep(0.1)
-def video(): 
+def video():
 	# capture frames from the camera
 	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 		# grab the raw NumPy array representing the image
 		image = frame.array
-		
+
 		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 		mask = cv2.inRange(hsv, greenLower, greenUpper)
 		mask = cv2.erode(mask, None, iterations=4)
 		mask = cv2.dilate(mask, None, iterations=4)
-		
+
 		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 		center = (0, 0)
-		
+
 		if(len(cnts) > 0):
 		    c = max(cnts, key=cv2.contourArea)
 		    ((x, y), radius) = cv2.minEnclosingCircle(c)
-		    
+
 		    if radius > 20:
 		        M = cv2.moments(c)
 		        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -80,30 +78,29 @@ def video():
 			cv2.circle(mask, (int(x), int(y)), int(radius),
 						(0, 255, 255), 2)
 			cv2.circle(mask, center, 5, (100, 100, 100), -1)
-			
-			min_y = np.min(np.min(c, axis=1), axis=0)[1]			
+
+			min_y = np.min(np.min(c, axis=1), axis=0)[1]
 			max_y = np.max(np.max(c, axis=1), axis=0)[1]
-			
+
 			vertical = max_y - min_y
-			
-			distance = (SIZE_BOX * FOCAL) / vertical			
+
+			distance = (SIZE_BOX * FOCAL) / vertical
 
 		if sensor.frontSensor(frindo) < 300 and \
 		   sensor.rightSensor(frindo) < 300 and \
 		   sensor.leftSensor(frindo) < 300:
 
-		
 			if(center[0] == 0):
 			    moves.turn_left(frindo, 50, BATTERY)
 			    time.sleep(0.2)
 			elif(center[0] > 0 and center[0] < 340):
 				z = np.divide(50, 368.) * (368 - center[0])
-				
+
 				print("Calibrating center")
 				moves.turn_left(frindo, int(z), BATTERY)
 			elif(center[0] > 396):
 				z = np.divide(50, 368.) * (center[0] - 368)
-				
+
 				print("Calibrating center")
 				moves.turn_right(frindo, int(z), BATTERY)
 			else:
@@ -111,18 +108,18 @@ def video():
 				moves.forwardv2(frindo, 25, BATTERY)
 		else:
 			print("Destination arrived!")
-		 
+
 		# show the frame
-		cv2.imshow(WIN_RF, mask)
-		key = cv2.waitKey(4) & 0xFF
-	 
+		# cv2.imshow(WIN_RF, mask)
+		# key = cv2.waitKey(4) & 0xFF
+
 		# clear the stream in preparation for the next frame
-		rawCapture.truncate(0)
-	 
+		# rawCapture.truncate(0)
+
 		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
+		#if key == ord("q"):
+		#	break
 
 
-	
-video()	
+
+video()
