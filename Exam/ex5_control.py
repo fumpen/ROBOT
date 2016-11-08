e2c1464551203dabb48b020fdf12fc87f942922f
anderslau <andrslsvndsn@gmail.com>
2016-11-08 21:07:12 +0000

    lort

--- Exam/ex5_control.py
+++ Exam/ex5_control.py
@@ -173,7 +173,6 @@ def find_landmark(inner_frindo, goal_number):
 # Initialize particles and update
 inner_frindo = FrindosInnerWorld()
 
-p.draw_world(particle.Particle(0,0,0), inner_frindo.getParticles, world)
 #particles = p.innit_particles(1000)
 #innit_est_pose = p.estimate_position(inner_frindo.getParticles())
 
