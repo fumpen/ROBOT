9b73c45ac83f09342be3705ad17bf400834005f0
anderslau <andrslsvndsn@gmail.com>
2016-11-09 21:09:37 +0000

    pray

--- Exam/ex5_control.py
+++ Exam/ex5_control.py
@@ -38,7 +38,6 @@ def rl_wuuut(dir):
         return 'left'
     else:
         return 'right'
-
 class FrindosInnerWorld:
     """This class keeps track of where the frindo thinks it is"""
     l_flag = dict
@@ -50,7 +49,157 @@ class FrindosInnerWorld:
     def __init__(self,
                  l_flag = LANDMARK,
                  l_coordinates = LANDMARK_COORDINATES,
-                 est_coordinate= INIT_POS,print "GOING FORWARD IN GOGOGO NOT KNOWING ANYTHING"
+                 est_coordinate= INIT_POS,
+                 particles = p.innit_particles(1000),
+                 current_goal = INIT_goal):
+
+        self.l_flag = l_flag
+        self.l_coordinates = l_coordinates
+        self.est_coordinate = est_coordinate
+        self.particles = particles
+        self.current_goal = current_goal
+
+    def update_l_flag(self, key):
+        print 'update l_flag: ' + str(key)
+        if 0 <= key <= 3:
+            print 'updated l_flag'
+            self.l_flag[key] = 1
+
+    def updateCurrentGoal(self, goal):
+        print '###################update_current_goal  with: ' + str(goal)
+        if 0 <= goal <= 3:
+            self.current_goal[goal] = 1
+        else:
+            raise
+        print 'update_current_goal after update: ' + str(self.current_goal)
+
+    def update_l_coordinates(self, coordinates):
+        self.l_coordinates = coordinates
+
+    def update_est_coordinate(self, est_coordinate):
+        self.est_coordinate = est_coordinate
+
+    def update_particles(self, particle):
+        self.particles = particle
+
+    def getFlag(self):
+        return self.l_flag
+
+    def getLCoordinates(self):
+        return self.l_coordinates
+
+    def getEstCoordinates(self):
+        return self.est_coordinate
+
+    def getParticles(self):
+        return self.particles
+
+    def getCurrentGoal(self):
+        return self.current_goal
+
+    def reset_landmarks(self):
+        x = 0
+        while x < 4:
+            self.l_flag[x] = 0
+            x += 1
+
+    def sum_of_observed_landmarks(self):
+        x = 0
+        for key, val in self.l_flag.iteritems():
+            x += val
+        return x
+
+    def update_from_update_particle(self, dicte):
+        self.update_particles(dicte['particles'])
+        self.update_l_flag(dicte['obs_obj'][3])
+        print "Print obs_obj[3]: "  + str(dicte['obs_obj'][3])
+        if dicte['obs_obj'][3] is not None:
+            print "control.update_from_update_particle.obs_obj[1]: " + str(dicte['obs_obj'][1]) + 'landmark: ' + str(dicte['obs_obj'][3])
+            if dicte['obs_obj'][1] < 75:
+                self.updateCurrentGoal(dicte['obs_obj'][3])
+        self.update_est_coordinate((dicte['est_pos'].getX(),
+                                    dicte['est_pos'].getY(),
+                                    dicte['est_pos'].getTheta()))
+
+
+#def make_observation(inner_frindo):
+#    ret_dict = p.update_particles(inner_frindo.getParticles(), cam, 0.0,
+#                              0.0, world, WIN_RF1, WIN_World)
+#    inner_frindo.update_from_update_particle(ret_dict)
+
+def turn(dir, deg, inner_frindo):
+    print '### turn ###'
+    print 'turn.deg: ' + str(deg)
+    print 'turn.dir: ' + str(dir)
+    m.turn_baby_turn(abs(np.divide(deg,2)), dir, frindo)
+    if dir == 'left':
+        ret_dict = p.update_particles(inner_frindo.getParticles(), cam, 0.0,
+                                      deg, world, WIN_RF1, WIN_World)
+    else:
+        ret_dict = p.update_particles(inner_frindo.getParticles(), cam, 0.0,
+                                    ((-1.0) * deg), world, WIN_RF1, WIN_World)
+    if ret_dict['obs_obj'][1]:
+        print 'observed landmark nr: ' + str(ret_dict['obs_obj'][3])
+    inner_frindo.update_from_update_particle(ret_dict)
+    sleep(0.2)
+    return ret_dict
+
+def go_forward(length, inner_frindo):
+    print '### go_forward ###'
+    print 'go_forward length: ' + str(length)
+
+    dist_driven = m.lige_gear_sensor(frindo, length)
+    ret_dict = p.update_particles(inner_frindo.getParticles(), cam, length,
+                                  0.0, world, WIN_RF1, WIN_World)
+    inner_frindo.update_from_update_particle(ret_dict)
+    sleep(0.2)
+    return dist_driven
+
+
+def find_landmark(inner_frindo, goal_number):
+    print '### find_landmark ###'
+    """attempt to find a given landmark"""
+    dest = p.where_to_go(inner_frindo.getEstCoordinates(),
+                         inner_frindo.getLCoordinates()[goal_number])
+    ret = turn(dest['dir'], dest['deg'], inner_frindo)
+    degrees_moved = 0.0
+    move_pr_turn = 35.0
+    goal = False
+    while degrees_moved <= 360:
+        degrees_moved += move_pr_turn+10
+        ret = turn('right', move_pr_turn, inner_frindo)
+        if ret['obs_obj'][3] is not None:
+            print "found :", ret['obs_obj']
+            if ret['obs_obj'][3] == goal_number:
+                goal = True
+                break
+        else:
+            ret = None
+    if goal:
+        dist = ret['obs_obj'][1]
+        if ret['obs_obj'][2] < 0:
+            dir = 'right'
+        else:
+            dir = 'left'
+        deg = ret['obs_obj'][2]
+    else:
+        dist = None
+        dir = None
+        deg = None
+    return {'dist': dist, 'dir': dir, 'deg': deg, 'goal': goal}
+
+
+def go_go_go(frindo, inner_frindo, goal_coordinates, goal):
+    print '### go_go_go ###'
+    """ go to a specific point (probably a landmark) """
+    """ Runs until robot thinks we're safely within range """
+    while p.dist_between_points(goal_coordinates, inner_frindo.getEstCoordinates()) > 50:
+        # (inner_frindo.getEstCoordinates()[0] not in range(goal[0]-50, goal[0]+50)) \
+        #     and (inner_frindo.getEstCoordinates()[1] not in range(goal[1]-50, goal[1]+50)):
+        dest = p.where_to_go(inner_frindo.getEstCoordinates(), goal_coordinates)
+        turn(dest['dir'], dest['deg'], inner_frindo)
+        if 50 <= (dest['dist'] - 50.0):
+            print "GOING FORWARD IN GOGOGO NOT KNOWING ANYTHING"
             turn(dest['dir'], dest['deg'], inner_frindo)
             ret = go_forward((dest['dist'] - 50.0), inner_frindo)
         else:
@@ -92,7 +241,6 @@ class FrindosInnerWorld:
             recon_area(15, 15, inner_frindo, goal)
         if inner_frindo.getFlag()[goal] == 1:
             break
-
 
 def recon_area(turns, deg, inner_frindo, goal_nr):
     print '### recon_area ###'
